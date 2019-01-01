524901b4e3910bbb73e735af197167acf3a89e1f
Nader Toukabri <nader.toukabri@gmail.com>
2019-01-01 15:55:12 +0100

    update lib

--- src/common/validate.js
+++ src/common/validate.js
@@ -9,7 +9,7 @@ export function exec(value, rules) {
     _error = [..._error, rule(value)];
   });
   return _error.length ? _error : null;
-};
+}
 
 export const PASSWORD_LENGTH = 8;
 export const EMAIL_PATTERN = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -19,8 +19,7 @@ export const required = (value) => !!value || 'Required.';
 export const password = (value) => value.length >= PASSWORD_LENGTH || `Min ${PASSWORD_LENGTH} characters`;
 
 export const email = (value) => {
-  const pattern = EMAIL_PATTERN;
-  return pattern.test(value) || 'Invalid email.';
+  return EMAIL_PATTERN.test(value) || 'Invalid email.';
 };
 
 export const username = email;
