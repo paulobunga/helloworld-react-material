931c27ace60c808ae38f7aa30005915b15c3eef8
Nader Toukabri <nader@emiketic.com>
2019-03-24 10:56:51 +0100

    update redux store setup

--- src/store/index.js
+++ src/store/index.js
@@ -14,93 +14,6 @@ import persistAdapter from 'redux-localstorage/lib/adapters/localStorage';
 
 import $state from './state';
 
-/**
- * define root reducer
- */
-
-// let reducer = combineReducers({
-//   Activity: $state.Activity.reducer,
-//   Auth: $state.Auth.reducer,
-//   // ...
-// });
-
-let reducer = combineReducers(
-  Object.entries($state).reduce((result, [name, substate]) => {
-    return substate.reducer
-      ? {
-        ...result,
-        [name]: substate.reducer,
-      }
-      : result;
-  }, {}),
-);
-
-/**
- * support loading persisted partial initial state
- */
-
-function merge(target, source) {
-  const result = { ...target };
-
-  for (const [key, value] of Object.entries(source)) {
-    if (value && Object.prototype.toString.call(value) === '[object Object]' && result[key]) {
-      result[key] = merge(result[key], value);
-    } else {
-      result[key] = value;
-    }
-  }
-
-  return result;
-}
-
-reducer = compose(mergePersistedState(merge))(reducer);
-
-/**
- * define persistence
- */
-
-// const persistSelector = (state) => ({
-//   Auth: $state.Auth.persister(state.Auth),
-//   // ...
-// });
-
-const persistSelector = (state) => Object.entries($state).reduce((result, [name, substate]) => {
-  return substate.persister
-    ? {
-      ...result,
-      [name]: substate.persister(state[name]),
-    }
-    : result;
-}, {});
-
-const persistStorage = compose((storage) => {
-  storage._put = storage.put;
-  storage.put = function (key, state, callback) {
-    storage._put(key, persistSelector(state), callback);
-  };
-  return storage;
-})(persistAdapter(window.localStorage));
-
-const persistEnhancer = persistState(persistStorage, 'redux');
-
-/**
- * define enhancers
- */
-
-const enhancerMiddleware = [thunk];
-
-if (process.env.NODE_ENV !== 'production') {
-  enhancerMiddleware.push(createLogger());
-}
-
-const composeEnhancers = global && global.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? global.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
-
-const enhancer = composeEnhancers(applyMiddleware(...enhancerMiddleware), persistEnhancer);
-
-/**
- * define store creation an initialization
- */
-
 let $store;
 
 export function getStore() {
@@ -108,43 +21,141 @@ export function getStore() {
 }
 
 export function setupStore() {
-  $store = createStore(reducer, enhancer);
+  /**
+   * Check and validate state definition in development mode
+   */
 
   if (process.env.NODE_ENV === 'development') {
-    global.$store = $store;
+    // const SKIP = ['MODULE', 'reducer', 'persister'];
+
+    Object.entries($state).forEach(([module, state]) => {
+      if (!state) {
+        console.warn(`$state.${module}: invalid descriptor`);
+        return;
+      }
+      if (!state.MODULE || state.MODULE !== module) {
+        console.warn(`$state.${module}: missing or invalid 'MODULE'`);
+      }
+      if (!state.reducer || typeof state.reducer !== 'function') {
+        console.warn(`$state.${module}: missing or invalid 'reducer'`);
+      }
+      if (state.persister && typeof state.persister === 'function') {
+        console.info(`$state.${module}: found 'persister'`);
+      }
+
+      // console.info(
+      //   `$state.${module}: found`,
+      //   Object.keys(state)
+      //     .filter((name) => SKIP.indexOf(name) === -1)
+      //     .map((name) => name),
+      // );
+    });
+  }
+
+  /**
+   * define root reducer
+   */
+
+  // let reducer = combineReducers({
+  //   Activity: $state.Activity.reducer,
+  //   Auth: $state.Auth.reducer,
+  //   // ...
+  // });
+
+  let reducer = combineReducers(
+    Object.entries($state).reduce((result, [name, substate]) => {
+      return substate.reducer
+        ? {
+          ...result,
+          [name]: substate.reducer,
+        }
+        : result;
+    }, {}),
+  );
+
+  /**
+   * support loading persisted partial initial state
+   */
+
+  function merge(target, source) {
+    const result = { ...target };
+
+    for (const [key, value] of Object.entries(source)) {
+      if (value && Object.prototype.toString.call(value) === '[object Object]' && result[key]) {
+        result[key] = merge(result[key], value);
+      } else {
+        result[key] = value;
+      }
+    }
+
+    return result;
+  }
+
+  reducer = compose(mergePersistedState(merge))(reducer);
+
+  /**
+   * define persistence
+   */
+
+  // const persistSelector = (state) => ({
+  //   Auth: $state.Auth.persister(state.Auth),
+  //   // ...
+  // });
+
+  const persistSelector = (state) => Object.entries($state).reduce((result, [name, substate]) => {
+    return substate.persister
+      ? {
+        ...result,
+        [name]: substate.persister(state[name]),
+      }
+      : result;
+  }, {});
+
+  const persistStorage = compose((storage) => {
+    storage._put = storage.put;
+    storage.put = function (key, state, callback) {
+      storage._put(key, persistSelector(state), callback);
+    };
+    return storage;
+  })(persistAdapter(window.localStorage));
+
+  const persistEnhancer = persistState(persistStorage, 'redux');
+
+  /**
+   * define enhancers
+   */
+
+  const enhancerMiddleware = [thunk];
+
+  if (process.env.NODE_ENV !== 'production') {
+    enhancerMiddleware.push(createLogger());
+  }
+
+  const composeEnhancers = global && global.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? global.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
+
+  const enhancer = composeEnhancers(applyMiddleware(...enhancerMiddleware), persistEnhancer);
+
+  /**
+   * expose $state
+   */
+
+  if (process.env.NODE_ENV === 'development') {
     global.$state = $state;
   }
 
+  /**
+   * create store
+   */
+
+  $store = createStore(reducer, enhancer);
+
+  /**
+   * expose $store
+   */
+
+  if (process.env.NODE_ENV === 'development') {
+    global.$store = $store;
+  }
+
   return $store;
-}
-
-/**
- * Check and validate state definition in development mode
- */
-
-if (process.env.NODE_ENV === 'development') {
-  // const SKIP = ['MODULE', 'reducer', 'persister'];
-
-  Object.entries($state).forEach(([module, state]) => {
-    if (!state) {
-      console.warn(`$state.${module}: invalid descriptor`);
-      return;
-    }
-    if (!state.MODULE || state.MODULE !== module) {
-      console.warn(`$state.${module}: missing or invalid 'MODULE'`);
-    }
-    if (!state.reducer || typeof state.reducer !== 'function') {
-      console.warn(`$state.${module}: missing or invalid 'reducer'`);
-    }
-    if (state.persister && typeof state.persister === 'function') {
-      console.info(`$state.${module}: found 'persister'`);
-    }
-
-    // console.info(
-    //   `$state.${module}: found`,
-    //   Object.keys(state)
-    //     .filter((name) => SKIP.indexOf(name) === -1)
-    //     .map((name) => name),
-    // );
-  });
 }
